c3afb1b05b912f243339a8f64b770f7a96e27b21
Raul Garcia <raul.jaime.garcia.espejo@gmail.com>
2020-12-04 17:57:08 -0600

    Updating login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,33 +16,37 @@
             @csrf
 
             <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-label for="email" value="{{ __('Correo') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-label for="password" value="{{ __('Contraseña') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <input id="remember_me" type="checkbox" class="form-checkbox" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Recuérdame') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        {{ __('¿Olvidaste la contraseña?') }}
                     </a>
                 @endif
 
+
                 <x-jet-button class="ml-4">
-                    {{ __('Login') }}
+                    {{ __('Ingresar') }}
                 </x-jet-button>
             </div>
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline"> ¿No estas registrado?</a>
+            @endif
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
