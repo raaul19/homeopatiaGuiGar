525adbe6970f33fe5774bfbdffe8efd2821bb315
Raul Garcia <raul.jaime.garcia.espejo@gmail.com>
2020-12-04 12:34:29 -0600

    Mutators in Products table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,18 @@ class Product extends Model
     use HasFactory;
     protected $table = "products";
     protected $fillable = ['name','description','price','photo'];
+
+    public function getDescriptionAttribute($value){
+        return ucfirst(strtolower($value));
+    }
+
+    /* propiedad virtual  tema_con_enlace
+    public function getTemaConEnlace(){
+        return $this->tema . "" . $this->enlace;
+    }
+    */
+
+    public function setNameAttribute($value){
+        $this->attributes['name'] = mb_strtoupper($value, "UTF-8");
+    }
 }
